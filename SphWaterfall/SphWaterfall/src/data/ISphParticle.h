2199de7d0e3911af4f0f769eed886f26265d9226
Tom <tcesko@t-online.de>
2017-11-13 20:23:56 +0100

    adds basic particle classes

---
+++ SphWaterfall/SphWaterfall/src/data/ISphParticle.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Vector3.h"
+
+class ISphParticle {
+	public:
+		Vector3 position;
+	private:
+};
---
+++ SphWaterfall/SphWaterfall/src/data/Vector3.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Vector3 {
+	public:
+		int x, y, z;
+		Vector3();
+		Vector3(int, int, int);
+		~Vector();
+	private:
+};
