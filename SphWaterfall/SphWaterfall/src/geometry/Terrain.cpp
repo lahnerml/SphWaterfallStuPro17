82b5729ccb3da83035e95e689ab024d91f0eaaf1
Magnus Specht <Magnus_Specht@gmx.net>
2017-12-12 18:26:29 +0100

    Modified Terrain Constructor

--- SphWaterfall/SphWaterfall/src/geometry/Terrain.cpp
+++ SphWaterfall/SphWaterfall/src/geometry/Terrain.cpp
@@ -1,6 +1,33 @@
 #pragma once
+
+#include <vector>
+
 #include "Terrain.h"
 
-Terrain::Terrain() {
+Terrain::Terrain(std::vector<Vector3> vertices, std::vector<Vector3> normals, std::vector<Vector3> faces) :
+	vertices(vertices),
+	normals(normals),
+	faces(faces)
+{
 
+}
+
+int Terrain::getVertexCount()
+{
+	return 0;
+}
+
+int Terrain::getFaceCount()
+{
+	return 0;
+}
+
+Vector3 Terrain::getVertexPosition(int index)
+{
+	return Vector3();
+}
+
+int Terrain::getFaceVertexId(int index, int vertexNr)
+{
+	return 0;
 }
--- SphWaterfall/SphWaterfall/src/geometry/Terrain.h
+++ SphWaterfall/SphWaterfall/src/geometry/Terrain.h
@@ -1,20 +1,22 @@
 #pragma once
+
+#include <vector>
+
 #include "../data/Vector3.h"
 #include "Face.h"
 
 class Terrain {
 public:
-	Terrain(int verticeCount, int faceCount);
+	Terrain(std::vector<Vector3> vertices, std::vector<Vector3> normals, std::vector<Vector3> faces);
 
 	int getVertexCount();
 	int getFaceCount();
 	Vector3 getVertexPosition(int index);
 	int getFaceVertexId(int index, int vertexNr);
 
-
-
 private:
 	Vector3 vertices[];
+	Vector3 normals[];
 	Face faces[];
 
 
