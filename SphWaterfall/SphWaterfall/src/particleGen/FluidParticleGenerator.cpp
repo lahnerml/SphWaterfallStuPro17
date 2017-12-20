f1f123c3abf5d03b44015c2153dd739e5d49c46c
Kennjy Marte <kennjymarte@web.de>
2017-12-20 12:57:14 +0100

    particleGen/FluidParticleGenerator.cpp

--- SphWaterfall/SphWaterfall/src/particleGen/FluidParticleGenerator.cpp
+++ SphWaterfall/SphWaterfall/src/particleGen/FluidParticleGenerator.cpp
@@ -1,7 +1,16 @@
 #pragma once
 #include "FluidParticleGenerator.h"
 
-FluidParticleGenerator::FluidParticleGenerator() {
+FluidParticleGenerator::FluidParticleGenerator(double disposalHeight, Vector3 spawn) {
+	this-> disposalHeight = disposalHeight;
+	this-> spawn = spawn;
+}
 
+ISphParticle FluidParticleGenerator::spawnParticle() {
+	//TODO
+}
+
+void FluidParticleGenerator::collectInactive() {
+	//TODO
 }
 
