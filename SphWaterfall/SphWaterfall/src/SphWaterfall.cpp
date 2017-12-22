cb15892ea2db65978ca1c3238b4aa5e0840fd369
Unknown <Tobias_Haller@outlook.de>
2017-12-22 19:57:45 +0100

    MPI Test Programm

--- SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
+++ SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include "mpi.h"
 
-int main()
-{
-	char a;
-	std::cout << "Hello World!" << std::endl;
-	std::cin >> a;
-	// Hallo Welt
+int main(int argc, char** argv) {
+	// Initialize the MPI environment
+	MPI_Init(NULL, NULL);
+
+	// Get the number of processes
+	int worldSize;
+	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
+	// Get the rank of the process
+	int worldRank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
+
+	// Get the name of the processor
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int nameLen;
+	MPI_Get_processor_name(processor_name, &nameLen);
+
+	// Print off a hello world message
+	printf("Hello world from processor %s, rank %d"
+		" out of %d processors\n",
+		processor_name, worldRank, worldSize);
+
+	// Finalize the MPI environment.
+	MPI_Finalize();
 }
