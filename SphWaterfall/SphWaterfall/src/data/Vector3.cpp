15e97724c81d50d33aac0c0b7ae92e7a0faadb9b
Tom <tcesko@t-online.de>
2018-01-01 23:28:34 +0100

    Adds implementation of a quintic WendlandKernel and some functions for Vector3

--- SphWaterfall/SphWaterfall/src/data/Vector3.cpp
+++ SphWaterfall/SphWaterfall/src/data/Vector3.cpp
@@ -1,16 +1,43 @@
 #pragma once
 #include "Vector3.h"
+#include <math.h>
 
-Vector3::Vector3() {
+Vector3::Vector3() :
+	x(0.0),
+	y(0.0),
+	z(0.0)
+{
 
 }
 
-Vector3::Vector3(float x, float y, float z) :
+Vector3::Vector3(double x, double y, double z) :
 	x(x),
 	y(y),
-	z(z) {
+	z(z) 
+{
+
 }
 
 Vector3::~Vector3() {
 
+}
+
+Vector3 operator*(const Vector3& vector, double factor) {
+	return Vector3(vector.x * factor, vector.y * factor, vector.z * factor);
+}
+
+Vector3 operator*(double factor, const Vector3& vector) {
+	return vector * factor;
+}
+
+Vector3 operator/(const Vector3& vector, double factor) {
+	return Vector3(vector.x / factor, vector.y / factor, vector.z / factor);
+}
+
+double Vector3::length() const {
+	return sqrt(x * x + y * y + z * z);
+}
+
+Vector3 Vector3::normalize() const {
+	return *this / length();
 }
