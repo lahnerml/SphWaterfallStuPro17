c1d2cc044ce4e864e50ee23d1c03fbc7bb861e16
Tom <tcesko@t-online.de>
2018-01-05 21:59:43 +0100

    Implements ParticleDomain methods and adds Vector3 functions.

--- SphWaterfall/SphWaterfall/src/data/Vector3.cpp
+++ SphWaterfall/SphWaterfall/src/data/Vector3.cpp
@@ -10,7 +10,7 @@ Vector3::Vector3() :
 
 }
 
-Vector3::Vector3(double x, double y, double z) :
+Vector3::Vector3(const double x, const double y, const double z) :
 	x(x),
 	y(y),
 	z(z) 
@@ -22,21 +22,36 @@ Vector3::~Vector3() {
 
 }
 
-Vector3 operator*(const Vector3& vector, double factor) {
+Vector3 operator*(const Vector3& vector, const double factor) {
 	return Vector3(vector.x * factor, vector.y * factor, vector.z * factor);
 }
 
-Vector3 operator*(double factor, const Vector3& vector) {
+Vector3 operator*(const double factor, const Vector3& vector) {
 	return vector * factor;
 }
 
-Vector3 operator/(const Vector3& vector, double factor) {
+Vector3 operator/(const Vector3& vector, const double factor) {
 	return Vector3(vector.x / factor, vector.y / factor, vector.z / factor);
 }
 
 Vector3 operator+(const Vector3& a, const Vector3& b)
 {
 	return Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+Vector3 operator-(const Vector3& a)
+{
+	return Vector3(-a.x, -a.y, -a.z);
+}
+
+Vector3 operator-(const Vector3& a, const Vector3& b)
+{
+	return Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+bool Vector3::all_dimensions_smaller_or_equal_then(const Vector3& b) const
+{
+	return x <= b.x && y <= b.y && z <= b.z;
 }
 
 double Vector3::length() const {
