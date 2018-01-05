87be431052395c00aa6217c01538586de92cc547
Tom <tcesko@t-online.de>
2018-01-05 21:59:43 +0100

    Implements ParticleDomain methods and adds Vector3 functions.

--- SphWaterfall/SphWaterfall/src/data/Vector3.h
+++ SphWaterfall/SphWaterfall/src/data/Vector3.h
@@ -4,15 +4,20 @@ class Vector3 {
 	public:
 		double x, y, z;
 		Vector3();
-		Vector3(double, double, double);
+		Vector3(const double, const double, const double);
 		~Vector3();
 
-		friend Vector3 operator*(const Vector3&, double);
-		friend Vector3 operator*(double, const Vector3&);
-		friend Vector3 operator/(const Vector3&, double);
+		friend Vector3 operator*(const Vector3&, const double);
+		friend Vector3 operator*(const double, const Vector3&);
+
+		friend Vector3 operator/(const Vector3&, const double);
 
 		friend Vector3 operator+(const Vector3&, const Vector3&);
+		friend Vector3 operator-(const Vector3&);
+		friend Vector3 operator-(const Vector3&, const Vector3&);
 
+		
+		bool all_dimensions_smaller_or_equal_then(const Vector3&) const;
 		double length() const;
 		Vector3 normalize() const;
 	private:
--- SphWaterfall/SphWaterfall/src/simulation/ParticleDomain.h
+++ SphWaterfall/SphWaterfall/src/simulation/ParticleDomain.h
@@ -7,15 +7,15 @@
 class ParticleDomain {
 public:
 	ParticleDomain();
-	ParticleDomain(ISphParticle particles[], Vector3, Vector3);
+	ParticleDomain(const ISphParticle* particles, const int& particle_count, const Vector3& center, const Vector3& size);
 	~ParticleDomain();
 
 	void updateParticlesOutsideDomain();
 
 private:
-	ISphParticle particles;
+	std::vector<ISphParticle> particles;
 	std::vector<ParticleDomain> neighbourDomains;
 	int particlesOutsideDomain;
-	Vector3 center;
-	Vector3 size;
+	Vector3 origin;
+	Vector3 dimensions;
 };
