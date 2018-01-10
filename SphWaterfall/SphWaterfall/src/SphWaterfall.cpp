5f40b4b1ff4d5c88290f46267cf239dfc4d7f324
Magnus Specht <Magnus_Specht@gmx.net>
2018-01-10 20:50:14 +0100

    Trying to add OpenMP

--- SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
+++ SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-
-int main()
-{
-	char a;
-	std::cout << "Hello World!" << std::endl;
-	std::cin >> a;
-	// Hallo Welt
-}
+#include <vector>
 
 void trim(std::string &str) {
 	int pos1 = str.find_first_not_of(" ");
@@ -16,17 +9,86 @@ void trim(std::string &str) {
 	str = str.substr(pos1, pos2 - pos1 + 1);
 }
 
-void readCommand()
+bool readParameter(char paramName, std::istringstream &parameters)
 {
-	std::string inputLine;
-	std::getline(std::cin, inputLine);
-	trim(inputLine);
-	
-	std::istringstream commandTokens(inputLine);
-	std::string command;
+	std::string nameToken, valueToken;
+	return (parameters >> nameToken && nameToken == "-" + paramName);
+}
 
-	while (commandTokens >> command)
+char readParameter(std::istringstream &parameterStream, std::vector<std::string> &values)
+{
+	std::string nameToken, valueToken;
+	
+	if (parameterStream.peek() != '-')
 	{
-		
+		std::cout << "Wrong token '" << parameterStream.peek() << "'." << std::endl;
+		return EFAULT;
 	}
+
+	// Read parameter name
+	if (!(parameterStream >> nameToken))
+		return EFAULT;
+
+	//Read values
+	while (parameterStream.peek() != '-' && parameterStream.peek() != EOF)
+	{
+		parameterStream >> valueToken;
+		values.push_back(valueToken);
+	}
+
+	std::cout << nameToken << std::endl;
+	return nameToken[1];
+}
+
+/* -_-_-_Comands Begin_-_-_- */
+
+void loadMesh(std::istringstream &parameters)
+{
+	std::vector<std::string> fileName;
+	if (readParameter(parameters, fileName) != 'p') {
+		std::cout << "Missing path parameter '-p'" << std::endl;
+	}
+}
+
+void showHelp()
+{
+	std::cout << "loadMesh -p" << std::endl;
+	std::cout << "particleGen [-w] [-f] [-e]" << std::endl;
+	std::cout << "moveShutter -t (-u/-d) [-l]" << std::endl;
+	std::cout << "simulate -s -e -r -g -m -t" << std::endl;
+	std::cout << "help" << std::endl;
+	std::cout << "exit" << std::endl;
+}
+
+/* -_-_-_Comands End_-_-_- */
+
+void readCommands()
+{
+	std::string inputLine, command;
+
+	while (true)
+	{
+		std::cout << std::endl << "Please enter a command or enter 'help' to show a list of all commands" << std::endl;
+		
+		std::getline(std::cin, inputLine);
+		trim(inputLine);
+		std::istringstream tokenStream(inputLine);
+
+		if (tokenStream >> command)
+		{
+			if (command == "loadMesh")
+				loadMesh(tokenStream);
+			else if (command == "help" || command == "?")
+				showHelp();
+			else if (command == "exit")
+				break;
+			else
+				std::cout << "Unknown command. Enter 'hlep' to view a list of all available commands." << std::endl;
+		}
+	}
+}
+
+int main()
+{
+	readCommands();
 }
