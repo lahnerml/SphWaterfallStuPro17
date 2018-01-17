cc7212406e670f2780f52b168f277ca9cd855d54
Kennjy Marte <kennjymarte@web.de>
2018-01-17 20:13:45 +0100

    StaticParticleGenerator.cpp

--- SphWaterfall/SphWaterfall/src/particleGen/StaticParticleGenerator.cpp
+++ SphWaterfall/SphWaterfall/src/particleGen/StaticParticleGenerator.cpp
@@ -1,6 +1,21 @@
 #pragma once
 #include "StaticParticleGenerator.h"
+#include "../data/SphParticle.h"
 
 StaticParticleGenerator::StaticParticleGenerator() {
 
 }
+
+std::vector<ISphParticle> StaticParticleGenerator :: generateStaticParticles(Terrain source) {
+	std::vector<ISphParticle> staticParticles;
+	for (int i = 0; i < source.getVertexCount(); i++) {
+		staticParticles.push_back(SphParticle(source.getVertexPosition(i)));
+		
+	}
+	return staticParticles;
+
+}
+
+
+
+
