b8fca728417d5afe14f5dd24a25a7fa7a1f026d4
Kennjy Marte <kennjymarte@web.de>
2018-01-17 18:17:33 +0100

    Statische Partikel generieren

--- SphWaterfall/SphWaterfall/src/particleGen/StaticParticleGenerator.cpp
+++ SphWaterfall/SphWaterfall/src/particleGen/StaticParticleGenerator.cpp
@@ -1,6 +1,22 @@
 #pragma once
 #include "StaticParticleGenerator.h"
+#include "../data/SphParticle.h"
 
 StaticParticleGenerator::StaticParticleGenerator() {
 
 }
+
+std::vector<ISphParticle> StaticParticleGenerator :: generateStaticParticles(Terrain source) {
+	std::vector<ISphParticle> staticParticle;
+	for (int i = 0; i < source.getVertexCount(); i++) {
+		SphParticle(Vector3);
+		staticParticle.push_back(Vector3);
+		
+	}
+	return staticParticle;
+
+}
+
+
+
+
--- SphWaterfall/SphWaterfall/src/particleGen/StaticParticleGenerator.h
+++ SphWaterfall/SphWaterfall/src/particleGen/StaticParticleGenerator.h
@@ -9,6 +9,4 @@ public:
 	StaticParticleGenerator();
 	std::vector<ISphParticle> generateStaticParticles(Terrain source);
 
-
-
 };
