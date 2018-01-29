cebc8fe98a983e2a4c7654bcf31c625a2bace2f4
T-Haller <32315230+T-Haller@users.noreply.github.com>
2018-01-29 18:41:59 +0100

    Merge pull request #5 from SpechtMagnus/M2-feature/CUI-extension
    CUI Implementation

--- SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
+++ SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
@@ -1,62 +1,79 @@
+#include <thread>
+
 #include "mpi.h"
 
 #include "cui/CUI.h"
 
-
 void loadMesh() {
-	printf("Command is loadMesh");
+	printf("Command is loadMesh\n");
 }
 
 void generateParticle() {
-	printf("command is particleGen");
+	printf("command is particleGen\n");
 }
 
 void moveShutter() {
-	printf("command is moveShutter");
+	printf("command is moveShutter\n");
 }
 
 void simulate() {
-	printf("command is simulate");
+	printf("command is simulate\n");
+}
+
+void render() {
+	printf("command is render\n");
 }
 
 int main(int argc, char** argv)
 {
 	MPI_Init(&argc, &argv);
 
-	int *buffer;
-	buffer = (int*)malloc(sizeof(int));
+	bool exit_programm = false;
+	std::thread cuiThread;
+	int *command_buffer;
+	command_buffer = (int*)malloc(sizeof(int));
 
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	CUI cui = CUI::CUI();
-	if (rank == 0) {
-		cui.readCommands(buffer);
-	}
+	while (!exit_programm) {
+		if (rank == 0) {
+			cuiThread = std::thread(CUI::readCommand, command_buffer);
+			cuiThread.join();
+		}
 
-	MPI_Bcast(buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	printf("Hello world from processor rank %d out of %d processors, buffer is %d\n", rank, world_size, buffer[0]);
+		MPI_Bcast(command_buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	if (*buffer == 1) {
-		loadMesh();
-	}
-	else if (*buffer == 2) {
-		generateParticle();
-	}
-	else if (*buffer == 3) {
-		moveShutter();
-	}
-	else if (*buffer == 4) {
-		simulate();
+		if (*command_buffer == 0) {
+			exit_programm = true;
+		} 
+		else if (rank != 0) {
+			printf("Hello world from processor rank %d out of %d processors, buffer is %d\n", rank, world_size, command_buffer[0]);
+
+			if (*command_buffer == 1) {
+				loadMesh();
+			} 
+			else if (*command_buffer == 2) {
+				generateParticle();
+			} 
+			else if (*command_buffer == 3) {
+				moveShutter();
+			} 
+			else if (*command_buffer == 4) {
+				simulate();
+			} 
+			else if (*command_buffer == 5) {
+				render();
+			}
+		}
 	}
 
 	if (rank == 0) {
 		system("pause");
 		MPI_Wait;
 	}
+
 	MPI_Finalize();
-
-
 }
--- SphWaterfall/SphWaterfall/src/cui/CUI.h
+++ SphWaterfall/SphWaterfall/src/cui/CUI.h
@@ -9,17 +9,7 @@
 #include "../simulation/SphManager.h"
 #include "../visualization/VisualizationManager.h"
 
-class CUI {
-public:
-	CUI();
+namespace CUI {
 	void trim(std::string &str);
-	bool readNextToken(std::queue<std::string> &tokens, std::string &nextToken);
-	bool readNextCombinedToken(std::queue<std::string> &tokens, std::string &nextToken);
-	void loadMesh(std::queue<std::string> &tokens);
-	void simulate(std::queue<std::string> &tokens);
-	void showHelp();
-	void readCommands(int* buffer);
-private:
-	std::string inputLine, command;
-	std::queue<std::string> tokens;
-};
+	void readCommand(int* command_buffer);
+}
