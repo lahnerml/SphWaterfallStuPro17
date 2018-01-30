0613f87f3f7802e78682eb9138dd00d9ddab26a6
Tom <tcesko@t-online.de>
2018-01-30 20:54:33 +0100

    Adds lazy Getters for all possible rims of a particle domain.

--- SphWaterfall/SphWaterfall/src/simulation/ParticleDomain.h
+++ SphWaterfall/SphWaterfall/src/simulation/ParticleDomain.h
@@ -26,6 +26,26 @@ public:
 	std::vector<ISphParticle> getRightRimParticles();
 	std::vector<ISphParticle> getFrontRimParticles();
 	std::vector<ISphParticle> getBackRimParticles();
+	std::vector<ISphParticle> getFrontBottomRimParticles();
+	std::vector<ISphParticle> getFrontTopRimParticles();
+	std::vector<ISphParticle> getBackBottomRimParticles();
+	std::vector<ISphParticle> getBackTopRimParticles();
+	std::vector<ISphParticle> getFrontLeftRimParticles();
+	std::vector<ISphParticle> getFrontRightRimParticles();
+	std::vector<ISphParticle> getBackLeftRimParticles();
+	std::vector<ISphParticle> getBackRightRimParticles();
+	std::vector<ISphParticle> getLeftBottomRimParticles();
+	std::vector<ISphParticle> getLeftTopRimParticles();
+	std::vector<ISphParticle> getRightBottomRimParticles();
+	std::vector<ISphParticle> getRightTopRimParticles();
+	std::vector<ISphParticle> getFrontLeftBottomRimParticles();
+	std::vector<ISphParticle> getFrontRightBottomRimParticles();
+	std::vector<ISphParticle> getFrontLeftTopRimParticles();
+	std::vector<ISphParticle> getFrontRightTopRimParticles();
+	std::vector<ISphParticle> getBackLeftBottomRimParticles();
+	std::vector<ISphParticle> getBackRightBottomRimParticles();
+	std::vector<ISphParticle> getBackLeftTopRimParticles();
+	std::vector<ISphParticle> getBackRightTopRimParticles();
 	void resetRimParticles();
 
 private:
@@ -39,7 +59,27 @@ private:
 	NullableWrapper<std::vector<ISphParticle>> right_rim_particles;
 	NullableWrapper<std::vector<ISphParticle>> top_rim_particles;
 	NullableWrapper<std::vector<ISphParticle>> bottom_rim_particles;
-
+	NullableWrapper<std::vector<ISphParticle>> front_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> front_top_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_top_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> front_left_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> front_right_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_left_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_right_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> left_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> left_top_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> right_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> right_top_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> front_left_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> front_right_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> front_left_top_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> front_right_top_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_left_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_right_bottom_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_left_top_rim_particles;
+	NullableWrapper<std::vector<ISphParticle>> back_right_top_rim_particles;
+	
 	int particles_outside_domain;
 	double rim_distance = 2;
 	Vector3 origin;
