a487daa0a008385f5606ee3dc7b937693aebec57
Magnus Specht <Magnus_Specht@gmx.net>
2018-01-30 16:56:18 +0100

    Extended SphParticle to include Type [FLUID/STATIC/MIRRORED]

--- SphWaterfall/SphWaterfall/src/data/SphParticle.cpp
+++ SphWaterfall/SphWaterfall/src/data/SphParticle.cpp
@@ -1,19 +1,38 @@
 #pragma once
 #include "SphParticle.h"
 
-SphParticle::SphParticle() {
-
+SphParticle::SphParticle() :
+	position(Vector3()),
+	velocity(Vector3()),
+	pType(ParticleType::FLUID)
+{
 }
 
-SphParticle::SphParticle(Vector3 position) {
-	this->position = position;
+SphParticle::SphParticle(Vector3 position) :
+	position(position),
+	pType(ParticleType::FLUID)
+{
 }
 
 SphParticle::SphParticle(Vector3 position, Vector3 velocity) :
 	position(position),
-	velocity(velocity) {
+	velocity(velocity),
+	pType(ParticleType::FLUID)
+{
+}
+
+SphParticle::SphParticle(Vector3 position, SphParticle::ParticleType pType) :
+	position(position),
+	velocity(Vector3()),
+	pType(pType)
+{
 }
 
 SphParticle::~SphParticle() {
 
+}
+
+SphParticle::ParticleType SphParticle::getParticleType()
+{
+	return this->pType;
 }
--- SphWaterfall/SphWaterfall/src/data/SphParticle.h
+++ SphWaterfall/SphWaterfall/src/data/SphParticle.h
@@ -3,12 +3,23 @@
 
 class SphParticle : public ISphParticle {
 	public:
+		enum ParticleType
+		{
+			FLUID,
+			STATIC,
+			MIRRORED
+		};
+
 		SphParticle();
 		SphParticle(Vector3);
 		SphParticle(Vector3, Vector3);
+		SphParticle(Vector3, SphParticle::ParticleType);
 		~SphParticle();
 
 		Vector3 position;
 		Vector3 velocity;
+
+		ParticleType getParticleType();
 	private:
+		const ParticleType pType;
 };
