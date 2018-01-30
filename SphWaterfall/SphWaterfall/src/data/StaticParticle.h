b83a2c005453df351da7d5205d6fa536b5fb8242
Magnus Specht <Magnus_Specht@gmx.net>
2018-01-30 17:51:29 +0100

    Added StaticParticle class

--- SphWaterfall/SphWaterfall/src/data/StaticParticle.h
+++ SphWaterfall/SphWaterfall/src/data/StaticParticle.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "ISphParticle.h"
 
-class StaticParticle : public ISphParticle {
+class StaticParticle : public IStaticParticle {
 public:
 	StaticParticle();
 	StaticParticle(Vector3);
 	~StaticParticle();
 
-	virtual ISphParticle::ParticleType getParticleType();
+	virtual Vector3 position();
+	virtual void position(Vector3 position);
+
+	virtual Vector3 velocity();
+	virtual void velocity(Vector3 velocity);
+
+	virtual IStaticParticle::ParticleType getParticleType();
 private:
-	Vector3 position;
+	Vector3 pos;
 };
