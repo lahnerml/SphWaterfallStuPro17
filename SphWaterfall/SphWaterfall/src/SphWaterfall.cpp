bb60332106f28e47d76d2813f8d721783aa941da
T-Haller <Tobias_Haller@outlook.de>
2018-02-03 16:48:05 +0100

    Revert "Velocity is now only available in FluidParticle, Particle classes must have a get, set is only for FluidParticle"
    This reverts commit 7918c71c4b9252e9cead1edc06fd09933d6eda69.

--- SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
+++ SphWaterfall/SphWaterfall/src/SphWaterfall.cpp
@@ -25,6 +25,7 @@ void simulate() {
 			for (int k = 0; k < 10; k++) {
 				SphParticle particle = FluidParticle(Vector3(i, j, k), Vector3(), 1.0);
 				particles.push_back(particle);
+				std::cout << particle.position.x << particle.position.y << particle.position.z << std::endl;
 			}
 		}
 	}
