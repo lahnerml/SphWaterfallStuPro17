67ccea321a374ba37ca8963135c2ceb5f568525c
Mark Czepan <mark.czepan@web.de>
2018-02-14 19:16:27 +0100

    .h addded

--- SphWaterfall/SphWaterfall/src/visualization/VideoWriter.h
+++ SphWaterfall/SphWaterfall/src/visualization/VideoWriter.h
@@ -7,12 +7,21 @@
 #include <opencv2\opencv.hpp>
 #include <opencv2\core\core.hpp>
 
-class video {
+using namespace std;
+
+class VideoWriter {
 public: 
-	video();
+	void videoWriter();
+	void imageWriter(Frame);
+	void createAlphaMat(Mat &mat, int pixel[3]);
 
 private:
 	vector<Mat> images;
 	VideoWriter outputVideo;
-
+	int height;
+	int width;
+	int Count;
+	int pixel[3];
+	string picturefiles;
+	Mat img;
 };
